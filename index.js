78a726e3c54c31c8b752738d5c6f99da8d84d826
femoxmed <femoxmed@gmail.com>
2019-08-14 03:05:08 -1100

    redis setup

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 6000 || process.env.PORT;
 
-// var redis = require("redis");
-// var session = require("express-session");
-// var redisStore = require("connect-redis")(session);
-// var client = redis.createClient();
+var redis = require("redis");
+var session = require("express-session");
+var redisStore = require("connect-redis")(session);
+var client = redis.createClient();
 
-// app.use(
-//   session({
-//     secret: "session_key",
-//     // create new redis store.
-//     store: new redisStore({
-//       host: "localhost",
-//       port: 6379,
-//       client: client,
-//       ttl: 260
-//     }),
-//     saveUninitialized: false,
-//     resave: false
-//   })
-// );
+app.use(
+  session({
+    secret: "session_key",
+    // create new redis store.
+    store: new redisStore({
+      host: "127.0.0.1",
+      port: 6379,
+      client: client,
+      ttl: 260
+    }),
+    saveUninitialized: false,
+    resave: false
+  })
+);
 
 require("./startup/database")(app);
 require("./startup/routes")(app);
